a46b728c190de3153e84e58a53c1f6ed2ad31efb
L-Karimi <luciahkei101@gmail.com>
2022-06-06 08:31:01 +0300

    modified files

--- app/src/main/java/dev/luciah/workoutlog/LoginActivity.kt
+++ app/src/main/java/dev/luciah/workoutlog/LoginActivity.kt
@@ -9,37 +9,40 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
-    lateinit var tilEmail: TextInputLayout
-    lateinit var tilPassword: TextInputLayout
-    lateinit var etEmail: EditText
-    lateinit var etPassword: EditText
+    lateinit var tilEmail1: TextInputLayout
+    lateinit var tilPassword2: TextInputLayout
+    lateinit var etEmail1: EditText
+    lateinit var etPassword2: EditText
     lateinit var btnLogin: Button
     lateinit var tvSignup: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btnLogin = findViewById(R.id.btnLogin)
+        tvSignup = findViewById(R.id.tvSignup)
         btnLogin.setOnClickListener {
+            validateLogin()
+
+        }
+        tvSignup.setOnClickListener {
             var intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
-            validateLogin()
         }
-        btnLogin = findViewById(R.id.btnLogin)
-        tilEmail = findViewById(R.id.tilEmail)
-        tilPassword = findViewById(R.id.tilPassword)
-        etEmail = findViewById(R.id.etEmail)
-        etPassword = findViewById(R.id.etPassword)
-        tvSignup = findViewById(R.id.tvSignup)
+        tilEmail1 = findViewById(R.id.tilEmail1)
+        tilPassword2 = findViewById(R.id.tilPassword2)
+        etEmail1 = findViewById(R.id.etEmail1)
+        etPassword2 = findViewById(R.id.etPassword2)
 
 
     }
 
     fun validateLogin() {
-        var email = etEmail.text.toString()
-        var password = etPassword.text.toString()
+        var email = etEmail1.text.toString()
+        var password = etPassword2.text.toString()
         if (email.isBlank()) {
-            tilEmail.error = "Email is required"
+            tilEmail1.error = "Email is required"
             if (password.isBlank()) {
-                tilPassword.error = "Password is required"
+                tilPassword2.error = "Password is required"
             }
 
 
--- app/src/main/java/dev/luciah/workoutlog/SignupActivity.kt
+++ app/src/main/java/dev/luciah/workoutlog/SignupActivity.kt
@@ -1,5 +1,6 @@
 package dev.luciah.workoutlog
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,65 +10,64 @@ import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class SignupActivity : AppCompatActivity() {
-lateinit var  tilFirstname:TextInputLayout
-lateinit var etFirstname:TextInputEditText
-lateinit var tilLastname:TextInputLayout
-lateinit var etLastname:TextInputEditText
-lateinit var tilPassword:TextInputLayout
-lateinit var etPassword: EditText
-lateinit var tilConfirmpassword:TextInputLayout
-lateinit var etConfirmpassword:EditText
-lateinit var btnSignup:Button
-lateinit var tvLogin:TextView
+    lateinit var tilFirstname: TextInputLayout
+    lateinit var etFirstname: TextInputEditText
+    lateinit var tilLastname: TextInputLayout
+    lateinit var etLastname: TextInputEditText
+    lateinit var tilEmail: TextInputLayout
+    lateinit var etPassword: EditText
+    lateinit var tilConfirmpassword: TextInputLayout
+    lateinit var etConfirmpassword: EditText
+    lateinit var btnSignup: Button
+    lateinit var tvLogin: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        btnSignup = findViewById(R.id.btnSignup)
+        tvLogin = findViewById(R.id.tvLogin)
         btnSignup.setOnClickListener {
 
         }
         tvLogin.setOnClickListener {
-            startActivity(this,LoginActivity::class.java)
+            var intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            validateSignup()
         }
-        tilFirstname=findViewById(R.id.tilFirstname)
-        tilLastname=findViewById(R.id.tilLastname)
+        tilFirstname = findViewById(R.id.tilFirstname)
+        tilLastname = findViewById(R.id.tilLastname)
 
-        tilPassword=findViewById(R.id.tilPassword)
-        tilConfirmpassword=findViewById(R.id.tilConfirmpassword)
-        etFirstname=findViewById(R.id.etFirstname)
-        etLastname=findViewById(R.id.etLastname)
-        etPassword=findViewById(R.id.etPassword)
-        etConfirmpassword=findViewById(R.id.etConfirmpassword)
-        tvLogin =findViewById(R.id.tvLogin)
+        tilEmail = findViewById(R.id.tilEmail)
+        tilConfirmpassword = findViewById(R.id.tilConfirmpassword)
+        etFirstname = findViewById(R.id.etFirstname)
+        etLastname = findViewById(R.id.etLastname)
+        etPassword = findViewById(R.id.etEmail)
+        etConfirmpassword = findViewById(R.id.etConfirmpassword)
 
     }
-    fun validateSignup () {
+
+    fun validateSignup() {
         var firstname = etFirstname.text.toString()
         var lastname = etLastname.text.toString()
-        var email = etEmail.text.toString()
         var password = etPassword.text.toString()
         var confirm_password = etConfirmpassword.text.toString()
 
-        if (firstname . isBlank()) {
+        if (firstname.isBlank()) {
             tilFirstname.error = "First name is required"
         }
-        if (lastname . isBlank()) {
-            tilLastname. error = "Last name is required"
+        if (lastname.isBlank()) {
+            tilLastname.error = "Last name is required"
         }
-        if (email . isBlank()) {
+
+        if (password.isBlank()) {
             tilEmail.error = "Email is required"
         }
-        if (password. isBlank()) {
-            tilPassword.error = "Password is required"
-        }
-        if (confirm_password. isBlank()) {
-            tilConfirmpassword.error = "confirm password required"
+        if (confirm_password.isBlank()) {
+            tilConfirmpassword.error = " password required"
 
         }
-        if (password != confirm_password) {
-            tilPassword.error = "wrong password"
 
-        }
+    }
 }
 
 

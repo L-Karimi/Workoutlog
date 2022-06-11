7fe7c055c8476b0c9e82b1fd0763f71c76b96336
L-Karimi <luciahkei101@gmail.com>
2022-06-11 08:40:32 +0300

    modified files

--- app/src/main/java/dev/luciah/workoutlog/SplashActivity.kt
+++ app/src/main/java/dev/luciah/workoutlog/SplashActivity.kt
@@ -7,7 +7,7 @@ import android.os.Bundle
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var intent = Intent(this,LoginActivity::class.java)
+        var intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
         finish()
     }

143f68c205e2c933e34bbe3b81184f41a419b09e
L-Karimi <luciahkei101@gmail.com>
2022-09-10 08:13:35 +0300

    added live data variables in viewmodel

--- app/src/main/java/dev/luciah/workoutlog/viewmodel/UserViewModel.kt
+++ app/src/main/java/dev/luciah/workoutlog/viewmodel/UserViewModel.kt
@@ -1,7 +1,12 @@
 package dev.luciah.workoutlog.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import dev.luciah.workoutlog.models.LoginResponse
+import dev.luciah.workoutlog.repository.UserRepository
 
-class UserViewModel : ViewModel (){
-
+class UserViewModel : ViewModel() {
+    val userRepository = UserRepository()
+    var loginResponseLiveData = MutableLiveData<LoginResponse>()
+    val loginErrorLiveData = MutableLiveData<String>()
 }
